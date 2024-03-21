d6d4ee6a446e77d617b4c5aa1a627758cdc293e6
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-21 14:21:41 -0300

    :sparkles: feat: added repository layer in task route

---
+++ src/task/repositories/taskRepository.ts
@@ -0,0 +1,25 @@
+class TaskRepository {
+
+  static create() {
+    return 'Create task';
+  }
+
+  static getAll() {
+    return 'Get all';
+  }
+
+  static getById() {
+    return 'getById';
+  }
+
+  static update() {
+    return 'update';
+  }
+
+  static delete() {
+    return 'delete';
+  }
+
+}
+
+export default TaskRepository;
