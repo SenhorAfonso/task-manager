7eeecb28d21a07b2304090a67804352d125d9d84
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-22 15:18:00 -0300

    :bug: fixed: error handling handle all errors again

--- src/middleware/ErrorHandlingMiddleware.ts
+++ src/middleware/ErrorHandlingMiddleware.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import StatusCodes from 'http-status-codes';
 import ApiError from '../errors/apiError';
 
 class ErrorHandlingMiddleware {
@@ -11,6 +12,8 @@ class ErrorHandlingMiddleware {
   ) {
     if (error instanceof ApiError) {
       res.status(error.status).json({ error: error.name });
+    } else {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
     }
   }
 
