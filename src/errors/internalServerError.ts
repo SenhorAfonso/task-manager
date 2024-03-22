b4e1e5d8d36130fc70bc61b332f0a0d6831af7d5
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-22 15:36:10 -0300

    :recycle: refactor: internalServerError fixed

--- src/errors/internalServerError.ts
+++ src/errors/internalServerError.ts
@@ -1,11 +1,13 @@
 import { StatusCodes } from 'http-status-codes';
 import ApiError from './apiError';
+import ApiErrorMessage from './enum/apiErrorMessage';
+import ApiErrorName from './enum/apiErrorName';
 
 class InternalServerError extends ApiError {
-  constructor(message: string = 'Internal Server Error') {
+  constructor(message: string = ApiErrorMessage.InternalServerError) {
     super(message);
     this.status = StatusCodes.INTERNAL_SERVER_ERROR;
-    this.name = 'Internal Server Error';
+    this.name = ApiErrorName.InternalServerError;
   }
 }
 
