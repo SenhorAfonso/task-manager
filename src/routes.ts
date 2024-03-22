4c61c7332824bb4ea260ff275a6f9cd07737c03b
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-22 12:23:46 -0300

    :sparkles: feat: validation middleware implemented in createTask route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import TaskController from './task/controller/taskController';
+import ValidationMiddleware from './task/middleware/ValidationMiddleware';
+import ValidateTask from './task/validations/validateTask';
 
 const taskRouter = express.Router();
 
-taskRouter.post('/task', TaskController.CreateTask);
+taskRouter.post('/task', [
+  ValidationMiddleware.ValidatePayload(ValidateTask.createTaks(), 'body')
+], TaskController.CreateTask);
 
 taskRouter.get('/task', TaskController.getTasks);
 
