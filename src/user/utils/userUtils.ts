df291a385c21c2704d769e5e7f82036643f70549
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-25 14:56:00 -0300

    :recycle: refactor(utils): utils classes were reorganized

--- src/user/utils/userUtils.ts
+++ src/user/utils/userUtils.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs';
+
 class UserUtils {
 
   static passwordsMatch(password: string, confirmPassword: string) {
@@ -5,6 +7,11 @@ class UserUtils {
       return false;
     }
     return true;
+  }
+
+  static passwordIsIncorrect(password: string, hashedPassword: string) {
+    const check = bcrypt.compareSync(password, hashedPassword);
+    return !check;
   }
 
 }
