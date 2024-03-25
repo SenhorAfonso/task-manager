9e4679c6008e5dc84881b9d24b6b5ca32b0d6917
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-25 13:44:33 -0300

    :sparkles: feat(user-repository): verification for duplicated email implemented

--- src/user/repositories/userRepository.ts
+++ src/user/repositories/userRepository.ts
@@ -1,14 +1,26 @@
 import StatusCodes from 'http-status-codes';
 import userSchema from '../schema/userSchema';
+import mongoose from 'mongoose';
+import IRegisterNewUser from '../DTOs/IRegisterNewUser';
+import BadRequestError from '../../errors/badRequestError';
 
 class UserRepository {
 
-  static async registerUser(registerUserPayload: any) {
+  static async registerUser(registerUserPayload: IRegisterNewUser) {
     const status: number = StatusCodes.OK;
     const message: string = 'User successfully registered!';
     const success: boolean = true;
+    const { email } = registerUserPayload;
 
-    const result = await userSchema.create(registerUserPayload);
+    let result: mongoose.Document | null;
+
+    result = await userSchema.findOne({ email });
+
+    if (result) {
+      throw new BadRequestError('The email entered is already registered!');
+    }
+
+    result = await userSchema.create(registerUserPayload);
 
     return { success, message, status, result };
   }
