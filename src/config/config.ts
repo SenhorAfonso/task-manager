6cd65d5b449ccd952bf5d90918dab6862b2eb52b
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-25 14:49:35 -0300

    :wrench: modified(.env): jwt_secrete_key added to .env

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,8 @@ dotenv.config();
 const serverConfig = {
   SERVER_PORT: process.env.SERVER_PORT,
   MONGO_URI: process.env.MONGO_URI,
-  BCRYPT_SALT: process.env.BCRYPT_SALT
+  BCRYPT_SALT: process.env.BCRYPT_SALT,
+  JWT_SECRETE_KEY: process.env.JWT_SECRETE_KEY
 };
 
 export default serverConfig;
