d073e766d53d69058040f4bb42a3b55975e70039
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-26 15:23:23 -0300

    :sparkles: feat(task-routes): authenticationMiddleware implemented

--- src/task/taskRoutes.ts
+++ src/task/taskRoutes.ts
@@ -2,21 +2,30 @@ import express from 'express';
 import TaskController from './controller/taskController';
 import ValidationMiddleware from '../middleware/ValidationMiddleware';
 import ValidateTask from './validations/validateTask';
+import AuthenticationMiddleware from '../middleware/AuthenticationMiddleware';
 
 const taskRouter = express.Router();
 
 taskRouter.post('/task', [
+  AuthenticationMiddleware.AuthenticateToken,
   ValidationMiddleware.ValidatePayload(ValidateTask.createTaks(), 'body')
 ], TaskController.CreateTask);
 
-taskRouter.get('/task', TaskController.getTasks);
+taskRouter.get('/task', [
+  AuthenticationMiddleware.AuthenticateToken
+], TaskController.getTasks);
 
-taskRouter.get('/task/:id', TaskController.getSingleTask);
+taskRouter.get('/task/:id', [
+  AuthenticationMiddleware.AuthenticateToken
+], TaskController.getSingleTask);
 
 taskRouter.put('/task/:id', [
+  AuthenticationMiddleware.AuthenticateToken,
   ValidationMiddleware.ValidatePayload(ValidateTask.updateTask(), 'body')
 ], TaskController.updateTask);
 
-taskRouter.delete('/task/:id', TaskController.deleteTask);
+taskRouter.delete('/task/:id', [
+  AuthenticationMiddleware.AuthenticateToken
+], TaskController.deleteTask);
 
 export default taskRouter;
