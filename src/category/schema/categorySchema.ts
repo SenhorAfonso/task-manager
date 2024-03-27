8e87b3f9f7187eb80d23a9bcfd6d8f41fbe1ab8f
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-27 13:21:22 -0300

    :sparkles: feat(category-schema): category schema added

---
+++ src/category/schema/categorySchema.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const MIN_NAME_LENGHT: number = 5;
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    min: [MIN_NAME_LENGHT, `Category name must be at least ${MIN_NAME_LENGHT} characteres lenght!`],
+    require: [true, 'Name is a required field!']
+  },
+  color: {
+    type: String,
+    enum: ['Green', 'Yellow', 'Orange', 'Red', 'Pink', 'Purple', 'Gray', 'Cyan', 'Blue'],
+    require: [true, 'Color is a required field!']
+  }
+});
+
+export default mongoose.model('categoryModel', categorySchema);
