ddbe72379fdae79e511f04501ec17caf7565706e
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-27 16:00:38 -0300

    :sparkles: feat(category-route): authentication middleware added to category route

--- src/category/categoryRoutes.ts
+++ src/category/categoryRoutes.ts
@@ -2,20 +2,29 @@ import express from 'express';
 import CategoryController from './controller/categoryController';
 import ValidationMiddleware from '../middleware/ValidationMiddleware';
 import ValidateCategory from './validations/validateCategory';
+import AuthenticationMiddleware from '../middleware/AuthenticationMiddleware';
 
 const categoryRoute = express.Router();
 
 categoryRoute.post('/category', [
+  AuthenticationMiddleware.AuthenticateToken,
   ValidationMiddleware.ValidatePayload(ValidateCategory.createCategory(), 'body')
 ], CategoryController.createCategory);
 
-categoryRoute.get('/category', CategoryController.getAllCategory);
+categoryRoute.get('/category', [
+  AuthenticationMiddleware.AuthenticateToken,
+], CategoryController.getAllCategory);
 
-categoryRoute.get('/category/:id', CategoryController.getSingleCategory);
+categoryRoute.get('/category/:id', [
+  AuthenticationMiddleware.AuthenticateToken
+], CategoryController.getSingleCategory);
 
-categoryRoute.put('/category/:id', CategoryController.updatecategory);
+categoryRoute.put('/category/:id', [
+  AuthenticationMiddleware.AuthenticateToken
+], CategoryController.updatecategory);
 
 categoryRoute.delete('/category/:id', [
+  AuthenticationMiddleware.AuthenticateToken,
   ValidationMiddleware.ValidatePayload(ValidateCategory.updateCategory(), 'body')
 ], CategoryController.deleteCategory);
 
