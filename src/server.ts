a5f11054402f7edd4a935b23c158df856ea5a649
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-28 13:13:47 -0300

    :recycle: refactor(api): database connection moved to server.ts as a method

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,15 @@ import taskRouter from './task/taskRoutes';
 import userRouter from './user/userRoutes';
 import categoryRoute from './category/categoryRoutes';
 import ErrorHandlingMiddleware from './middleware/ErrorHandlingMiddleware';
+import DataBase from './database/connectDB';
 
 class Server {
-  public server: express.Express;
+  public server: express.Application;
 
   constructor() {
     this.server = express();
     this.middlewares();
+    this.database();
   }
 
   middlewares() {
@@ -20,6 +22,11 @@ class Server {
     this.server.use('/api/v1/', categoryRoute);
     this.server.use(ErrorHandlingMiddleware.errorHandler);
   }
+
+  async database() {
+    await DataBase.connect();
+  }
+
 }
 
 export default new Server().server;
