2eb79a0d597eb5a8acef0ccc9e9bfdc67e3da837
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-28 14:08:35 -0300

    :sparkles: feat(user-routes): validationMiddleware implemented at user routes

--- src/user/userRoutes.ts
+++ src/user/userRoutes.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import UserController from './controller/userController';
+import ValidationMiddleware from '../middleware/ValidationMiddleware';
+import ValidateUser from './validations/validateUser';
 
 const userRouter = express.Router();
 
-userRouter.post('/user/signup', UserController.register);
+userRouter.post('/user/signup', [
+  ValidationMiddleware.ValidatePayload(ValidateUser.registerUser(), 'body')
+] , UserController.register);
 
-userRouter.post('/user/login', UserController.login);
+userRouter.post('/user/login', [
+  ValidationMiddleware.ValidatePayload(ValidateUser.loginUser(), 'body')
+], UserController.login);
 
 export default userRouter;
