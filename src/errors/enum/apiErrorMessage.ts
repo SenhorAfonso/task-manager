eaec80c21d3557da379bc489904b2ef13f90172f
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-28 10:58:55 -0300

    :bug: fixed(api-error): typo fixed

--- src/errors/enum/apiErrorMessage.ts
+++ src/errors/enum/apiErrorMessage.ts
@@ -1,5 +1,5 @@
 enum ApiErrorMessage {
-  InternalServerError = 'A unknow error ocurred. Please try again later!',
+  InternalServerError = 'A unknown error ocurred. Please try again later!',
   BadRequestError = 'The payload is invalid.',
   NotFoundError = 'No items matches with the query.',
   UnauthorizedAccessError = 'You are not allowed to access this content.',
