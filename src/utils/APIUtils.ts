f81291a1ebe6716afb3601d0cdaff1cda8621946
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-29 14:57:10 -0300

    :recycle: refactor(api-utils): getCategoryID removed from APIUtils

--- src/utils/APIUtils.ts
+++ src/utils/APIUtils.ts
@@ -1,10 +1,5 @@
-import categorySchema from '../category/schema/categorySchema';
-import InternalServerError from '../errors/internalServerError';
-import NotFoundError from '../errors/notFoundError';
 import IAuthenticatedDocument from '../interface/IAuthenticatedDocument';
 import IQuerySearch from '../task/interface/IQuerySearch';
-import type mongoDocument from '../types/mongoDocument';
-import type nullable from '../types/nullable';
 
 class APIUtils {
   static isEmpty(target: any[] | any): boolean {
@@ -25,8 +20,8 @@ class APIUtils {
       queryObject.category = query.category;
     }
 
-    if (query.taskStatus) {
-      queryObject.taskStatus = query.taskStatus;
+    if (query.status) {
+      queryObject.status = query.status;
     }
 
     if (query.expiration) {
@@ -34,23 +29,6 @@ class APIUtils {
     }
 
     return queryObject;
-  }
-
-  static async getCategoryID(name: string): Promise<string> {
-    let categoryDoc: nullable<mongoDocument>;
-
-    try {
-      categoryDoc = await categorySchema.findOne({ name });
-    } catch (error) {
-      throw new InternalServerError('A unknown error ocurred during searching for category name. Please try again later.');
-    }
-
-    if (APIUtils.isEmpty(categoryDoc)) {
-      throw new NotFoundError(`The category ${name} does not exist!`);
-    }
-
-    return categoryDoc!.id;
-
   }
 
 }
