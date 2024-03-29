f405c4997ea5c6bcab980be52888bbc07b6d23ff
Pedro Afonso <sirafonso005896@outlook.com>
2024-03-29 15:18:25 -0300

    :sparkles: feat(task-interface): status added to taskDocument interface

--- src/task/interface/taskDocument.ts
+++ src/task/interface/taskDocument.ts
@@ -1,6 +1,7 @@
 import type mongoDocument from '../../types/mongoDocument';
 
 interface taskDocument extends mongoDocument {
-  categoryID: string
+  categoryID: string,
+  status: 'pending'| 'in-progress'| 'finished'
 }
 export default taskDocument;
