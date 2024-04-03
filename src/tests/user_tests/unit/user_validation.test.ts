b65bf2d060a670361ad95a1f12d13b7c156f9770
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-03 12:59:59 -0300

    :test_tube: test(user-unit): test for email and password field at login route implemented

--- src/tests/user_tests/unit/user_validation.test.ts
+++ src/tests/user_tests/unit/user_validation.test.ts
@@ -174,3 +174,74 @@ describe('Validation payload for users sign-up route.', () => {
   });
 
 });
+describe('Validation payload for users sign-in route.', () => {
+
+  describe('Validate email field and value', () => {
+
+    it('Should return a "email must be a valid email" error', () => {
+      const userPayload = {
+        username: 'Pedro',
+        weight: 75,
+        email: 'pedroafonsogmail.com',
+        password: 'password123',
+        confirmPassword: 'password123'
+      };
+
+      const error = TestUtils.validateObject(ValidateUser.loginUser(), userPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"email" must be a valid email');
+      expect(error.path).toStrictEqual(['email']);
+    });
+
+    it('Should return a "email is required" error', () => {
+      const userPayload = {
+        username: 'Pedro',
+        weight: 75,
+        invalid: 'pedroafonso@gmail.com',
+        password: 'password123',
+        confirmPassword: 'password123'
+      };
+
+      const error = TestUtils.validateObject(ValidateUser.loginUser(), userPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"email" is required');
+      expect(error.path).toStrictEqual(['email']);
+    });
+
+  });
+
+  describe('Validate password field and value', () => {
+
+    it('Should return an "invalid pattern " error to passwords with less than 6 characters', () => {
+      const userPayload = {
+        username: 'Pedro',
+        weight: 75,
+        email: 'pedroafonso@gmail.com',
+        password: 'pass',
+        confirmPassword: 'password123'
+      };
+
+      const error = TestUtils.validateObject(ValidateUser.loginUser(), userPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"password" with value "pass" fails to match the required pattern: /^[a-zA-Z0-9]{6,30}$/');
+      expect(error.path).toStrictEqual(['password']);
+    });
+
+    it('Should return a "password is required" error', () => {
+      const userPayload = {
+        username: 'Pedro',
+        weight: 75,
+        email: 'pedroafonso@gmail.com',
+        invalid: 'password123',
+        confirmPassword: 'password123'
+      };
+
+      const error = TestUtils.validateObject(ValidateUser.loginUser(), userPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"password" is required');
+      expect(error.path).toStrictEqual(['password']);
+    });
+
+  });
+
+});
