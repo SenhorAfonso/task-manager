84c04c3ed2da945a6da9da91a034b27c4d9e0835
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-03 10:03:17 -0300

    :sparkles: feat(task-service): get task info method implemented

--- src/task/service/taskService.ts
+++ src/task/service/taskService.ts
@@ -45,6 +45,30 @@ class TaskService {
     return { success, status, message, result };
   }
 
+  static async getTasksInfo(userID: string) {
+    let { success, status, message, result } = await TaskRepository.getAllTasks(userID);
+
+    const finishedTasks = this.getTaskByStatus('finished', result);
+    const biggestDescription = this.getBiggestDescription(result);
+    const oldestTask = this.getOldestTask(result);
+
+    return {
+      success,
+      status,
+      message,
+      result: {
+        conclusion_avg: result.length / finishedTasks.length,
+        biggest_description: biggestDescription,
+        oldest_task: oldestTask
+      }
+    };
+  }
+
+  static async aggregateTask(userID: string) {
+    const result  = await TaskRepository.aggregateTaskByCategory(userID);
+    return result;
+  }
+
   static getSingleTask(taskId: ITaskId, userID: string) {
     const result = TaskRepository.getSingleTask(taskId, userID);
     return result;
@@ -84,6 +108,35 @@ class TaskService {
 
     filteredArray = taskArray.filter(element => element.date_conclusion.toLocaleDateString() === expirationDate);
     return filteredArray;
+  }
+
+  static getBiggestDescription(taskArray: taskDocument[]) {
+    let biggestLenght: number = 0;
+    let res: taskDocument = taskArray[0];
+
+    taskArray.forEach(element => {
+      if (element.description.length > biggestLenght) {
+        biggestLenght = element.description.length;
+        res = element;
+      }
+      return res;
+    });
+
+    return res;
+  }
+
+  static getOldestTask(taskArray: taskDocument[]) {
+    let oldest = taskArray[0].date_creation.getTime();
+    let result: taskDocument = taskArray[0];
+
+    taskArray.forEach(element => {
+      if (element.date_creation.getTime() < oldest) {
+        oldest = element.date_conclusion.getTime();
+        result = element;
+      }
+    });
+
+    return result;
   }
 
 }
