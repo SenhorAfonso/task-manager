6b2da45b263f284c0d66e5f2bc8b922afcdcaa80
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-03 13:18:41 -0300

    :test_tube: test(category-unit): test for color field implemented

--- src/tests/category_tests/unit/category_validation.test.ts
+++ src/tests/category_tests/unit/category_validation.test.ts
@@ -31,4 +31,32 @@ describe('Payload validation for create new category route', () => {
 
   });
 
+  describe('Validate color field and value', () => {
+
+    it('Should return a "color must be one of" error', () => {
+      const categoryPayload = {
+        name: 'Do the dishes',
+        color: 'invalid',
+      };
+
+      const error = TestUtils.validateObject(ValidateCategory.createCategory(), categoryPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"color" must be one of [Green, Yellow, Orange, Red, Pink, Purple, Gray, Cyan, Blue]');
+      expect(error.path).toStrictEqual(['color']);
+    });
+
+    it('Should return a "color is required" error', () => {
+      const categoryPayload = {
+        name: 'Do the dishes',
+        invalid: 'Red',
+      };
+
+      const error = TestUtils.validateObject(ValidateCategory.createCategory(), categoryPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"color" is required');
+      expect(error.path).toStrictEqual(['color']);
+    });
+
+  });
+
 });
