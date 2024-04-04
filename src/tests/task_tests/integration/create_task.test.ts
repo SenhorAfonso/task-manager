df236461678d490da471f628f4f49771a0625a91
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-04 15:16:49 -0300

    :test_tube: test(task-integration): test for check if non existent category at create task route return status 404 implemented

--- src/tests/task_tests/integration/create_task.test.ts
+++ src/tests/task_tests/integration/create_task.test.ts
@@ -99,4 +99,46 @@ describe('Chech task\'s create route http responses', () => {
     expect(response.body.success).toBeTruthy();
   });
 
+  it('Should return 404 when the category do not exist', async () => {
+    const userSignUpPayload = {
+      username: 'Pedro',
+      email: 'pedroafonso@gmail.com',
+      weight: 75,
+      password: 'password123',
+      confirmPassword: 'password123'
+    };
+
+    await request(server)
+      .post('/api/v1/user/signup')
+      .send(userSignUpPayload);
+
+    const userLoginPayload = {
+      email: 'pedroafonso@gmail.com',
+      password: 'password123'
+    };
+
+    const loginResponse = await request(server)
+      .post('/api/v1/user/login')
+      .send(userLoginPayload);
+
+    const { token } = loginResponse.body.data;
+
+    const createTaskPayload = {
+      title: 'Finish the homework',
+      description: 'The homework is the API',
+      type: 'Homework',
+      category: 'Graduation',
+      status: 'pending'
+    };
+
+    const response = await request(server)
+      .post('/api/v1/task')
+      .send(createTaskPayload)
+      .auth(token, { type: 'bearer' });
+
+    expect(response.status).toBe(StatusCodes.NOT_FOUND);
+    expect(response.body.error.message).toBe('The category do not exist!');
+    expect(response.body.success).toBeFalsy();
+  });
+
 });
