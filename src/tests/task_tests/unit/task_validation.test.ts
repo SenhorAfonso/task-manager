674425e08ab078822087eec0df35e4bc7edeb405
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-04 11:05:50 -0300

    :test_tube: test(task-unit): test for title field implemented

---
+++ src/tests/task_tests/unit/task_validation.test.ts
@@ -0,0 +1,44 @@
+import ValidateTask from '../../../task/validations/validateTask';
+import TestUtils from '../../utils/testUtils';
+
+describe('Payload validation for create new task route', () => {
+
+  describe('Validate title field and value', () => {
+
+    it('Should return a "title lenght must be at least 5 characters long" error', () => {
+
+      const taskPayload = {
+        title: 'Fini',
+        description: 'I have to finish the test suit',
+        type: 'Homework',
+        category: 'Graduation',
+        status: 'pending'
+      };
+
+      const error = TestUtils.validateObject(ValidateTask.createTaks(), taskPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"title" length must be at least 5 characters long');
+      expect(error.path).toStrictEqual(['title']);
+
+    });
+
+    it('Should return a "title is required" error', () => {
+
+      const taskPayload = {
+        invalid: 'Finish the API',
+        description: 'I have to finish the test suit',
+        type: 'Homework',
+        category: 'Graduation',
+        status: 'pending'
+      };
+
+      const error = TestUtils.validateObject(ValidateTask.createTaks(), taskPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"title" is required');
+      expect(error.path).toStrictEqual(['title']);
+
+    });
+
+  });
+
+});
