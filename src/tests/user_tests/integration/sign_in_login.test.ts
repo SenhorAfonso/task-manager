f59ab264e20bcefac9e143fd3898fe445a7bd9cf
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-04 14:18:03 -0300

    :test_tube: test(user-integration): test for check if not registered email at login route return status 404 implemented

--- src/tests/user_tests/integration/sign_in_login.test.ts
+++ src/tests/user_tests/integration/sign_in_login.test.ts
@@ -49,4 +49,32 @@ describe('Check user\'s login route\'s http responses', () => {
     expect(response.body.message).toBe('User successfully logged in');
   });
 
+  it('Should return 200 status code when the user log-in successfully', async () => {
+
+    const userSignUpPayload = {
+      username: 'Pedro',
+      email: 'pedroafonso@gmail.com',
+      weight: 75,
+      password: 'password123',
+      confirmPassword: 'password123'
+    };
+
+    await request(server)
+      .post('/api/v1/user/signup')
+      .send(userSignUpPayload);
+
+    const userLoginPayload = {
+      email: 'pedroafonso1@gmail.com',
+      password: 'password123'
+    };
+
+    const response = await request(server)
+      .post('/api/v1/user/login')
+      .send(userLoginPayload);
+
+    expect(response.status).toBe(StatusCodes.NOT_FOUND);
+    expect(response.body.success).toBeFalsy();
+    expect(response.body.error.message).toBe('There is no user with the provided email!');
+  });
+
 });
