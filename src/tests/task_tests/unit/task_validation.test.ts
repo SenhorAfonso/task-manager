4d6eb6d9ff35a0cf48cadd3bc8b17d763f98dc54
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-04 11:09:18 -0300

    :test_tube: test(task-unit): test for description field implemented

--- src/tests/task_tests/unit/task_validation.test.ts
+++ src/tests/task_tests/unit/task_validation.test.ts
@@ -41,4 +41,59 @@ describe('Payload validation for create new task route', () => {
 
   });
 
+  describe('Validate description field and value', () => {
+
+    it('Should return a "description lenght must be at least 5 characters long" error', () => {
+
+      const taskPayload = {
+        title: 'Finish the API',
+        description: 'inva',
+        type: 'Homework',
+        category: 'Graduation',
+        status: 'pending'
+      };
+
+      const error = TestUtils.validateObject(ValidateTask.createTaks(), taskPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"description" length must be at least 5 characters long');
+      expect(error.path).toStrictEqual(['description']);
+
+    });
+
+    it('Should return a "description is not allowed to be empty" error', () => {
+
+      const taskPayload = {
+        title: 'Finish the API',
+        description: '',
+        type: 'Homework',
+        category: 'Graduation',
+        status: 'pending'
+      };
+
+      const error = TestUtils.validateObject(ValidateTask.createTaks(), taskPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"description" is not allowed to be empty');
+      expect(error.path).toStrictEqual(['description']);
+
+    });
+
+    it('Should return a "description is required" error', () => {
+
+      const taskPayload = {
+        invalid: 'Finish the API',
+        description: 'I have to finish the test suit',
+        type: 'Homework',
+        category: 'Graduation',
+        status: 'pending'
+      };
+
+      const error = TestUtils.validateObject(ValidateTask.createTaks(), taskPayload).error!.details[0]!;
+
+      expect(error.message).toMatch('"title" is required');
+      expect(error.path).toStrictEqual(['title']);
+
+    });
+
+  });
+
 });
