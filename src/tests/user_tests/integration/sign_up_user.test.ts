9112b507a97e31e41cd3be4b696a69c9efaadb6b
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-04 13:24:14 -0300

    :test_tube: test(user-integration): test for 400 http response at register route implemented

--- src/tests/user_tests/integration/sign_up_user.test.ts
+++ src/tests/user_tests/integration/sign_up_user.test.ts
@@ -34,9 +34,32 @@ describe('Check user\'s sign-up route\'s http response', () => {
       .post('/api/v1/user/signup')
       .send(userSignUpPayload);
 
-    expect(response.status).toBe(StatusCodes.OK);
+    expect(response.status).toBe(StatusCodes.CREATED);
     expect(response.body.success).toBeTruthy();
     expect(response.body.message).toBe('User successfully registered!');
+
+  });
+
+  it('Should return 400 status code when the email is already registered', async () => {
+    const userSignUpPayload = {
+      username: 'Pedro',
+      email: 'pedroafonso@gmail.com',
+      weight: 75,
+      password: 'password123',
+      confirmPassword: 'password123'
+    };
+
+    await request(server)
+      .post('/api/v1/user/signup')
+      .send(userSignUpPayload);
+
+    const response = await request(server)
+      .post('/api/v1/user/signup')
+      .send(userSignUpPayload);
+
+    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+    expect(response.body.success).toBeFalsy();
+    expect(response.body.error.message).toBe('The email entered is already registered!');
 
   });
 
