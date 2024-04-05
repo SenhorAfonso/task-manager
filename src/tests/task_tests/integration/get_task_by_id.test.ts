14f1acaf2790b3636bb7f892f9d5a8884a157677
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-04 23:58:05 -0300

    :test_tube: test(task-integration): test to check if get by id route return 401 when not authenticated implemented

---
+++ src/tests/task_tests/integration/get_task_by_id.test.ts
@@ -0,0 +1,63 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import StatusCodes from 'http-status-codes';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import taskSchema from '../../../task/schema/taskSchema';
+import server from '../../../server';
+import TestUtils from '../../../utils/testUtils';
+import userSchema from '../../../user/schema/userSchema';
+import categorySchema from '../../../category/schema/categorySchema';
+
+let mongoServer: MongoMemoryServer;
+
+describe('Chech task\'s create route http responses', () => {
+  beforeEach(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const mongoURI = mongoServer.getUri();
+    mongoose.connect(mongoURI);
+  });
+
+  afterEach(async () => {
+    await userSchema.collection.drop();
+    await categorySchema.collection.drop();
+    await taskSchema.collection.drop();
+    await mongoServer.stop();
+    await mongoose.connection.close();
+  });
+
+  it('Should return 401 when the route is not authenticated', async () => {
+    const registerUserPayload = {
+      username: 'Pedro',
+      email: 'pedroafonso@gmail.com',
+      weight: 75,
+      password: 'password123',
+      confirmPassword: 'password123'
+    };
+
+    const token = await TestUtils.loginUser(registerUserPayload);
+
+    const createCategoryPayload = {
+      name: 'Graduation',
+      color: 'Red'
+    };
+
+    await TestUtils.createCategory(token, createCategoryPayload);
+
+    const createTaskPayload = {
+      title: 'Finish the homework',
+      description: 'The homework is the API',
+      type: 'Homework',
+      category: 'Graduation',
+      status: 'pending',
+    };
+
+    const taskID = await TestUtils.creatTask(token, createTaskPayload);
+
+    const response = await request(server)
+      .get(`/api/v1/task/${taskID}`);
+
+    expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+
+  });
+
+});
