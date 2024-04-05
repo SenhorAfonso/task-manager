7cee35daf150fa940ee0e6a2904841e690a4d3fe
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-04 23:54:20 -0300

    :truck: moved(api): tests utils moved to api utils folder

---
+++ src/utils/testUtils.ts
@@ -0,0 +1,50 @@
+import Joi from 'joi';
+import request from 'supertest';
+import server from '../server';
+import IRegisterNewUser from '../user/DTOs/IRegisterNewUser';
+
+class TestUtils {
+
+  static validateObject(
+    validationSchema: Joi.ObjectSchema,
+    target: object
+  ) {
+    const res = validationSchema.validate(target, { abortEarly: false });
+    return res;
+  }
+
+  static async loginUser(payload: IRegisterNewUser): Promise<string> {
+    const { email, password } = payload;
+
+    await request(server)
+      .post('/api/v1/user/signup')
+      .send(payload);
+
+    const response = await request(server)
+      .post('/api/v1/user/login')
+      .send({ email, password });
+
+    const { token } = response.body.data;
+    return token;
+  }
+
+  static async createCategory(token: string, payload: any): Promise<void> {
+    await request(server)
+      .post('/api/v1/category')
+      .send(payload)
+      .auth(token, { type: 'bearer' });
+  }
+
+  static async creatTask(token: string, payload: any): Promise<string> {
+    const response = await request(server)
+      .post('/api/v1/task')
+      .send(payload)
+      .auth(token, { type: 'bearer' });
+
+    const taksID = response.body.result._id;
+    return taksID;
+  }
+
+}
+
+export default TestUtils;
