ba3bbc872d8b07db90da711eb3eaa4b25b72058a
Pedro Afonso <sirafonso005896@outlook.com>
2024-04-28 18:00:53 -0300

    :bug: fixed(register-user): fix test hooks

--- src/tests/user_tests/integration/sign_up_user.test.ts
+++ src/tests/user_tests/integration/sign_up_user.test.ts
@@ -6,17 +6,21 @@ import userSchema from '../../../user/schema/userSchema';
 import server from '../../../server';
 
 let mongoServer: MongoMemoryServer;
+let mongoURI: string;
 
 describe('Check user\'s sign-up route\'s http response', () => {
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create();
-    const mongoURI = mongoServer.getUri();
+    mongoURI = mongoServer.getUri();
     await mongoose.connect(mongoURI);
   });
 
   afterEach(async () => {
-    await userSchema.collection.drop();
+    await userSchema.collection.deleteMany({});
+  });
+
+  afterAll(async () => {
     await mongoServer.stop();
     await mongoose.connection.close();
   });
@@ -98,6 +102,28 @@ describe('Check user\'s sign-up route\'s http response', () => {
     expect(response.body.success).toBeFalsy();
     expect(response.body.errors).toBeInstanceOf(Array);
     expect(response.body.errors).toHaveLength(5);
+
+  });
+
+  it('Should return 500 when the database is not connected', async () => {
+    await mongoose.connection.close();
+
+    const userSignUpPayload = {
+      username: 'Pedro',
+      email: 'pedroafonso@gmail.com',
+      weight: 75,
+      password: 'password123',
+      confirmPassword: 'password123'
+    };
+
+    const response = await request(server)
+      .post('/api/v1/user/signup')
+      .send(userSignUpPayload);
+
+    await mongoose.connect(mongoURI);
+    expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(response.body.error.message).toBe('An internal server error ocurred. Please try again later.');
+    expect(response.body.success).toBeFalsy();
 
   });
 
